4e43e49522ad3f69d64c908b6a268016de162e05
U170709005 <almiragurkan@posta.mu.edu.tr>
2022-01-21 15:40:34 +0300

    updated

---
+++ app/components/filters/filters.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+import { View } from 'react-native';
+import SectionedMultiSelect from 'react-native-sectioned-multi-select';
+import { Icon } from "../icon/icon"
+
+const items = [
+  {
+    name: 'Fruits',
+    id: 0,
+    children: [
+      {
+        name: 'Apple',
+        id: 10,
+      },
+      {
+        name: 'Strawberry',
+        id: 17,
+      },
+      {
+        name: 'Pineapple',
+        id: 13,
+      },
+      {
+        name: 'Banana',
+        id: 14,
+      },
+      {
+        name: 'Watermelon',
+        id: 15,
+      },
+      {
+        name: 'Kiwi fruit',
+        id: 16,
+      },
+    ],
+
+  },
+  {
+    name: 'Vegetables',
+    id: 1,
+    children: [
+      {
+        name: 'Apple',
+        id: 96,
+      },
+      {
+        name: 'Strawberry',
+        id: 77,
+      },
+
+    ],
+
+  },
+
+];
+
+export const Filters = () => {
+
+  const [selectedItems, setSelectedItems] = useState([])
+
+  const onSelectedItemsChange = (selectedItems) => {
+    setSelectedItems([{ selectedItems }]);
+    console.log(selectedItems.id)
+  };
+
+    return (
+      <View>
+        <SectionedMultiSelect
+          items={items}
+          IconRenderer={Icon}
+          uniqueKey="id"
+          subKey="children"
+          selectText="Filtrele"
+          showDropDowns={true}
+          readOnlyHeadings={true}
+          onSelectedItemsChange={onSelectedItemsChange}
+          selectedItems={selectedItems}
+        />
+      </View>
+
+    );
+
+
+}

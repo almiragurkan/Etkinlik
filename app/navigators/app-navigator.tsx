2121114c7a8690a447d5f038482bf6efe11e24ea
Kadir Özen <kadirozen@hotmail.com>
2022-03-07 12:12:32 +0300

    StackNavigator removed DrawerNavigator will be used.

--- app/navigators/app-navigator.tsx
+++ app/navigators/app-navigator.tsx
@@ -1,108 +1,66 @@
-/**
- * The app navigator (formerly "AppNavigator" and "MainNavigator") is used for the primary
- * navigation flows of your app.
- * Generally speaking, it will contain an auth flow (registration, login, forgot password)
- * and a "main" flow which the user will use once logged in.
- */
+
 import React from "react"
 import { useColorScheme } from "react-native"
 import { NavigationContainer, DefaultTheme, DarkTheme } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createDrawerNavigator } from "@react-navigation/drawer"
 import {
   WelcomeScreen,
-  DemoScreen,
-  DemoListScreen,
-  ActivityListScreen,
   ActivityDetailScreen,
-  ProfileScreen,
-  FiltersScreen,
+
 } from "../screens"
 import { navigationRef, useBackButtonHandler } from "./navigation-utilities"
 import { Filters } from "../components"
-import { useStores } from "../models"
 import { color } from "../theme"
 
-
-/**
- * This type allows TypeScript to know what routes are defined in this navigator
- * as well as what properties (if any) they might take when navigating to them.
- *
- * If no params are allowed, pass through `undefined`. Generally speaking, we
- * recommend using your MobX-State-Tree store(s) to keep application state
- * rather than passing state through navigation params.
- *
- * For more information, see this documentation:
- *   https://reactnavigation.org/docs/params/
- *   https://reactnavigation.org/docs/typescript#type-checking-the-navigator
- */
 export type NavigatorParamList = {
   welcome: undefined
   demo: undefined
   activityList: undefined
   demoList: undefined
-  activityDetail: {activityId}
+  activityDetail: { activityId }
   profile: undefined
-  filter:undefined
-  filters:undefined
+  filter: undefined
+  filters: undefined
 }
 
-// Documentation: https://reactnavigation.org/docs/stack-navigator/
-const Stack = createNativeStackNavigator<NavigatorParamList>()
-const Drawer = createDrawerNavigator();
-const AppStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-      initialRouteName="welcome"
-    >
-      <Stack.Screen name="welcome" component={WelcomeScreen} />
-      <Stack.Screen name="demo" component={DemoScreen} />
-      <Stack.Screen name="demoList" component={DemoListScreen} />
-      <Stack.Screen name="activityList" component={ActivityListScreen} />
-      <Stack.Screen name="activityDetail" component={ActivityDetailScreen} />
-      <Stack.Screen name="profile" component={ProfileScreen} />
-      <Stack.Screen name="filter" component={FiltersScreen} />
-    </Stack.Navigator>
-  )
-}
+const Drawer = createDrawerNavigator<NavigatorParamList>()
+
+
 const AppDrawer = () => {
   return (
     <Drawer.Navigator
       screenOptions={{
-        headerShown: true,
+        headerShown: false,
         headerTitle: "ETKİNLİKLER",
-        headerStyle:{backgroundColor:color.palette.lighterGrey},
-        headerTitleAlign:"center"
+        headerStyle: { backgroundColor: color.palette.lighterGrey },
+        headerTitleAlign: "center",
       }}
       initialRouteName="welcome"
-      drawerContent={props => <Filters/>}
+      drawerContent={ props => <Filters/>}
     >
       <Drawer.Screen name="welcome" component={WelcomeScreen} />
       <Drawer.Screen name="filter" component={Filters} />
-      <Stack.Screen name="activityDetail" component={ActivityDetailScreen} />
+      <Drawer.Screen name="activityDetail" component={ActivityDetailScreen} />
     </Drawer.Navigator>
   )
 }
 
 
-interface NavigationProps extends Partial<React.ComponentProps<typeof NavigationContainer>> {}
+interface NavigationProps extends Partial<React.ComponentProps<typeof NavigationContainer>> {
+}
 
 export const AppNavigator = (props: NavigationProps) => {
   const colorScheme = useColorScheme()
-  const {activityStore} = useStores()
   useBackButtonHandler(canExit)
-  console.log(activityStore.isDetail)
+  // const routeName = getActiveRouteName(navigationRef.getRootState())
+  console.log(navigationRef.getRootState())
   return (
     <NavigationContainer
       ref={navigationRef}
       theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
       {...props}
     >
-
-      {activityStore.isDetail ? <AppStack /> : <AppDrawer /> }
+    <AppDrawer />
     </NavigationContainer>
   )
 }
